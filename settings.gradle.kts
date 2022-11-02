df419199fbf1f0d909720c928cc1716d8f5cd2ee
Junhyung <vjh0107@naver.com>
2022-11-03 08:28:28 +0900

    Refactor, Chore: clean settings.gradle.kts, refactor modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,16 @@
 rootProject.name = "BarcodeGradlePlugins"
 
-val modulesDir = "plugins"
-
-include("project-extensions")
-
-if (System.getenv("JITPACK") == "true") {
-    include("plugins:common")
-} else {
+fun includeAll(modulesDir: String) {
     file("${rootProject.projectDir.path}/${modulesDir.replace(":", "/")}/").listFiles()?.forEach { modulePath ->
         include("${modulesDir.replace("/", ":")}:${modulePath.name}")
     }
+}
+
+include("project-extensions")
+include("plugin-common")
+
+if (System.getenv("JITPACK") != "true") {
+    includeAll("plugins")
 }
 
 pluginManagement {
